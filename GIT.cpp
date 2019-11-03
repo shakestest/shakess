a2ac21af5e2e9a51dfd6bd57ffd4ba8e031b7d64
igorstoner <lenovoingvarr@gmail.com>
2019-11-04 00:04:42 +0200

    first commit

---
+++ GIT.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <math.h>
+int main()
+{
+	double a, b, c, P;
+
+	//Введення початкових даних
+	printf("input A: ");
+	scanf_s("%lf", &a);
+	printf("input B: ");
+	scanf_s("%lf", &b);
+	printf("input C: ");
+	scanf_s("%lf", &c);
+	printf("input N: ");
+	P = a + b + c;
+	b = a * a;
+	//Виведення результату
+	printf("P= %lf\n", P);
+	system("pause");
+	return 0;
+}
